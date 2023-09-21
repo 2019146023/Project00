08176b9fb32dc0fa967dc4a4816b175ac1281d4e
YouJisang <sjf115@naver.com>
2023-09-21 20:43:32 +0900

    Test

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include <Arduino.h>
+#include <SSD1306.h>
+SSD1306         display(0x3c, 4, 5, GEOMETRY_128_32);
 
-// put function declarations here:
-int myFunction(int, int);
-
+int val = 0; // AD 변환값을 저장할 변수
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+ Serial.begin(115200);
+ display.init();
+ display.flipScreenVertically();
+ display.setFont(ArialMT_Plain_16);
+ display.drawString(10, 10, "Hello World");
+ display.display();
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+ val = analogRead(0); // 아날로그 0번 핀 (VR) 값을 입력
+ Serial.println(val);
+ delay(50);
 }
